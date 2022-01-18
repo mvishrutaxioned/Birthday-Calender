9fce3e0dac92be2298863266677c9d9ffa90f4d9
VishrutMargam <vishrutm@axioned.com>
2022-01-18 18:47:44 +0530

    added displayData functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(() => {
     // check number functionality
     function checkNumber(elem, str) {
         if(elem.length == 0) displayError('Above field is required', `${str}`)
-        else if (regexNum.test(elem)) displayError(`Please enter valid year`, `${str}`)
+        else if (regexNum.test(elem) || elem.length != 4) displayError(`Please enter valid year`, `${str}`)
         else {
             displaySuccess(`${str}`)
             return 1
@@ -66,11 +66,41 @@ $(document).ready(() => {
         let data = JSON.parse(json);
         let dateArr = [];
         let newDateArr = [];
+        let nameInitial = [];
+        let jsonData = [];
 
         $(data).filter((i, e) => dateArr.push(e.birthday));
+        $(data).filter((i, e) => {
+            const fullName = e.name.split(' ');
+            const initials = fullName.shift().charAt(0) + fullName.pop().charAt(0);
+            nameInitial.push(initials)
+        });
         $(dateArr).filter((i, e) => newDateArr.push(`${e.slice(0,6)}${yearInput}`));
 
-        displayData(newDateArr)
+        class JsonDate {
+            constructor(n, d) {
+                this.n = n;
+                this.d = d;
+            }
+        }
+        for (var i=0; i<data.length; i++) {
+            const obj = new JsonDate(nameInitial[i], newDateArr[i]);
+            jsonData.push(obj);
+        }
+        displayData(jsonData)
+    }
+
+    const displayData = (data) => {
+        $(`.card`).addClass('empty');
+        $(`.card .names`).html('<i class="far fa-frown-open"></i>');
+
+        for(var i=0; i<data.length; i++) {
+            const myDay = new Date(data[i].d).getDay() + 1
+            const content = `<li>${data[i].n}</li>`;
+
+            $(`.card:nth-child(${myDay}) .names i`).before(content);
+            $(`.card:nth-child(${myDay})`).removeClass('empty');
+        }
     }
 
     // on form submit    
@@ -86,5 +116,4 @@ $(document).ready(() => {
             console.log('nope')
         }
     })
-
 })
